44bce30c395d5424cbe6412fb4bf65a79957c98b
Latemarch <73418661+Latemarch@users.noreply.github.com>
2023-01-29 17:30:11 +0900

    getting title from mock data

--- youtube/src/pages/Videos.jsx
+++ youtube/src/pages/Videos.jsx
@@ -1,7 +1,26 @@
+import { useQuery } from '@tanstack/react-query'
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import VideoCard from '../components/VideoCard'
+import { search } from '../apis/youtube'
 
 export default function Videos() {
   const { keyword } = useParams()
-  return <div>Video {keyword ? `${keyword}` : 'hot'}</div>
+  const { isLoading, error, data: videos } = useQuery(['videos', keyword], () =>
+    search(keyword),
+  )
+  return (
+    <>
+      <div>Video {keyword ? `${keyword}` : 'hot'}</div>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Something is wrong</p>}
+      {videos && (
+        <ul>
+          {videos.map((video) => (
+            <VideoCard key={video.id} video={video} />
+          ))}
+        </ul>
+      )}
+    </>
+  )
 }
